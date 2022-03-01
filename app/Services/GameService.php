f556c9463aa4fb58a6cf52ae99b150eb37c1040b
cesar molina <cesar.molina.torres.05@gmail.com>
2022-03-01 11:50:38 -0500

    New: GameService::class

---
+++ app/Services/GameService.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+namespace App\Services;
+
+use App\Models\Game;
+
+class GameService extends Game
+{
+    /**
+     *
+     * clase de servicio
+     *
+     *
+     *
+     *
+     * **/
+    public function filter()
+    {
+    }
+
+    public function store($game)
+    {
+    }
+
+
+}
