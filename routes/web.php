37d971b1610761a8e887238abcc462c9ea09c112
cesar molina <cesar.molina.torres.05@gmail.com>
2022-03-01 11:39:42 -0500

    NEW: route web

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,32 @@
 <?php
 
+use App\Http\Controllers\Api\GameController;
+use App\Http\Controllers\RegisterController;
+use App\Models\Game;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [GameController::class,'index'])->name('home');
+
+
+ Route::controller(GameController::class)->group(function () {
+     Route::get('/games', 'index')->name('games.index');
+     Route::get('/games/{game:slug}', 'edit');
+     Route::post('/games/{id}', 'edit');
+     Route::post('/games', 'store');
+     Route::put('/games/{id}', 'update')->name('games.update');
+     Route::delete('/games/{id}', 'destroy')->name('games.destroy');
+     Route::get('state/{state}', [GameController::class, 'state']);
+ });#->middleware('admin');
+
+
+Route::get('authors/{author:name}', function(User $author) {
+    return view('games.index', [
+        'games' => $author->games
+    ]);
 });
+
+
+Route::get('register', [RegisterController::class, 'create']);
+Route::post('register', [RegisterController::class, 'store']);
