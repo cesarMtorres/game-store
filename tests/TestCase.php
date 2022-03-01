3ddf67f20d4d29e8eac9f32f8c010d5081140f47
cesar molina <cesar.molina.torres.05@gmail.com>
2022-03-01 11:49:06 -0500

    TEST: testCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,27 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, RefreshDatabase;
+    protected $faker;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->faker = Faker::create();
+        Artisan::call('migrate'); // run the migrations
+    }
+
+    // rollback migrations
+    public function tearDown():void
+    {
+        Artisan::call('migrate::rollback');
+        parent::tearDown();
+    }
 }
