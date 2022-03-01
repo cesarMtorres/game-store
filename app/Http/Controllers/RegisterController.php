7f04e04fe7379c5224cfcf7252c5e6f02ed38cd1
cesar molina <cesar.molina.torres.05@gmail.com>
2022-03-01 14:05:24 -0500

    New: registerUser add: session regenerator for bypass attack

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class RegisterController extends Controller
 {
@@ -22,7 +23,12 @@ class RegisterController extends Controller
             'password' => 'required|min:7|max:255',
         ]);
 
-        User::create($attributes);
-        return redirect('/');
+        $user = User::create($attributes);
+
+
+        Auth::login($user);
+
+
+        return redirect('/')->with('success','Your Account was be Created.');
     }
 }
